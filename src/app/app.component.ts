dd689c37a093b79a2b09811b4d8ea2e9396b83c6
Kaycodes11 <kayydraws@gmail.com>
2023-07-15 23:56:16 +0530

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h2>This is app component</h2>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'luis ramirez';
-  imgURL = 'https://picsum.photos/id/237/500/500';
-  currentDate = new Date();
-  cost = 2000;
-  temperature = 25.22;
-  pizza = {toppings: ['pepperoni', 'bacon'], size: 'large'}
 
-  getTitle () {
-    return `this is angular ${this.title}`;
-  }
-
-  changeImage(event: KeyboardEvent ) {
-    this.imgURL = (event.target as HTMLInputElement).value;
-  }
-
-  logImg(event: string) {
-    console.log(event);
+  ngOnInit() {
+    console.log('initial render');
   }
 }
